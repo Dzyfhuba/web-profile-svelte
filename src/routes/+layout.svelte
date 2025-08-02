f9c8a01b81195f502a94a554f37e14584cee3a1b
Dzyfhuba <hafidz21ub@gmail.com>
2025-08-02 15:25:04 +0700

    feat: add navigation bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,24 @@
 	import { href } from '$lib/helpers/url-helper';
 
 	let { children } = $props();
+
+	const menuItems = [
+		{
+			label: 'About',
+			href: href('/')
+		},
+		{
+			label: 'Portfolio',
+			href: href('/about')
+		},
+		{
+			label: 'Blog',
+			href: href('/blog')
+		}
+	];
 </script>
 
-<nav class="p-4">
+<nav class="mx-auto max-w-xl p-4">
 	<div class="flex items-center justify-between">
 		<!-- Light mode logo -->
 		<a href={href('/')} class="flex">
@@ -22,12 +37,25 @@
 				class="hidden h-12 w-12 rounded-full dark:block"
 				alt="hafidz ubadillah"
 			/>
-			<div class="text-base-content">
+			<!-- <div class="text-base-content">
 				<h1 class="text-2xl font-bold">Hafidz Ubadillah</h1>
 				<p class="text-sm">Web Developer</p>
-			</div>
+			</div> -->
 		</a>
-		<div class="dropdown dropdown-end">
+
+		<!-- Desktop menu -->
+		<div class="hidden sm:flex">
+			<ul class="menu menu-horizontal p-0">
+				{#each menuItems as item}
+					<li>
+						<a href={item.href}>{item.label}</a>
+					</li>
+				{/each}
+			</ul>
+		</div>
+
+		<!-- Mobile menu -->
+		<div class="dropdown dropdown-end sm:hidden">
 			<div tabindex="0" role="button" class="btn btn-circle btn-ghost">
 				<svg
 					class="h-6 w-6"
@@ -45,9 +73,11 @@
 				</svg>
 			</div>
 			<ul class="dropdown-content menu z-1 mt-3 w-52 menu-sm rounded-box bg-base-100 p-2 shadow">
-				<li><a href={href('/')}>Homepage</a></li>
-				<li><a href={href('/projects')}>Projects</a></li>
-				<li><a href={href('/blogs')}>Blogs</a></li>
+				{#each menuItems as item}
+					<li>
+						<a href={item.href}>{item.label}</a>
+					</li>
+				{/each}
 			</ul>
 		</div>
 	</div>
